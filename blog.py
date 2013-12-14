ba4146dea72c6a5fc195fba22accb1845f079944
ymao <ym943@nyu.edu>
2013-12-13 21:57:16 -0500

    add google user login/logout

--- blog.py
+++ blog.py
@@ -6,6 +6,7 @@ import webapp2
 import jinja2
 
 from google.appengine.ext import db
+from google.appengine.api import users
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
@@ -29,16 +30,13 @@ def render_post(response, post):
     response.out.write('<b>' + post.subject + '</b><br>')
     response.out.write(post.content)
 
-class MainPage(BlogHandler):
-  def get(self):
-      self.write('Hello, Yixia!')
-
-##### blog stuff
+# blog stuff
 
 def blog_key(name = 'default'):
     return db.Key.from_path('blogs', name)
 
 class Post(db.Model):
+    author = db.UserProperty()
     subject = db.StringProperty(required = True)
     content = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
@@ -48,10 +46,18 @@ class Post(db.Model):
         self._render_text = self.content.replace('\n', '<br>')
         return render_str("post.html", p = self)
 
-class BlogFront(BlogHandler):
+class MainPage(BlogHandler):
     def get(self):
         posts = db.GqlQuery("select * from Post order by created desc limit 10")
-        self.render('front.html', posts = posts)
+        
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        self.render('front.html', posts = posts, url = url, url_linktext = url_linktext)
 
 class PostPage(BlogHandler):
     def get(self, post_id):
@@ -71,74 +77,17 @@ class NewPost(BlogHandler):
     def post(self):
         subject = self.request.get('subject')
         content = self.request.get('content')
+        author = users.get_current_user()
 
         if subject and content:
-            p = Post(parent = blog_key(), subject = subject, content = content)
+            p = Post(parent = blog_key(), subject = subject, content = content, author = author)
             p.put()
             self.redirect('/blog/%s' % str(p.key().id()))
         else:
             error = "Type in subject and content, please!"
-            self.render("newpost.html", subject=subject, content=content, error=error)
-
-USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-def valid_username(username):
-    return username and USER_RE.match(username)
-
-PASS_RE = re.compile(r"^.{3,20}$")
-def valid_password(password):
-    return password and PASS_RE.match(password)
-
-EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
-def valid_email(email):
-    return not email or EMAIL_RE.match(email)
-
-class Signup(BlogHandler):
-
-    def get(self):
-        self.render("signup-form.html")
-
-    def post(self):
-        have_error = False
-        username = self.request.get('username')
-        password = self.request.get('password')
-        verify = self.request.get('verify')
-        email = self.request.get('email')
-
-        params = dict(username = username,
-                      email = email)
-
-        if not valid_username(username):
-            params['error_username'] = "That's not a valid username."
-            have_error = True
-
-        if not valid_password(password):
-            params['error_password'] = "That wasn't a valid password."
-            have_error = True
-        elif password != verify:
-            params['error_verify'] = "Your passwords didn't match."
-            have_error = True
-
-        if not valid_email(email):
-            params['error_email'] = "That's not a valid email."
-            have_error = True
-
-        if have_error:
-            self.render('signup-form.html', **params)
-        else:
-            self.redirect('/blog/welcome?username=' + username)
-
-class Welcome(BlogHandler):
-    def get(self):
-        username = self.request.get('username')
-        if valid_username(username):
-            self.render('welcome.html', username = username)
-        else:
-            self.redirect('/blog/signup')
+            self.render("newpost.html", subject=subject, content=content, author = author, error=error)
 
 app = webapp2.WSGIApplication([('/', MainPage),
-                               ('/blog/signup', Signup),
-                               ('/blog/welcome', Welcome),
-                               ('/blog/?', BlogFront),
                                ('/blog/([0-9]+)', PostPage),
                                ('/blog/newpost', NewPost),
                                ],
